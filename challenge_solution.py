9a9b315961194997396961b885ccc478aa5925b8
bldanHS <dbarbatlorincz@hearsaycorp.com>
2022-11-30 20:32:21 +0100

    commit 3

--- challenge_solution.py
+++ challenge_solution.py
@@ -1,7 +1,7 @@
 # Solution to programming challenge for Learning Python course
 # LinkedIn Learning Python course by Joe Marini
 #
-
+""" 
 def is_palindrome(teststr):
     # use the slice trick to reverse the string
     if teststr == teststr[::-1]:
@@ -27,3 +27,30 @@ while (run):
             newstr += x
 
     print("Palindrome test:", is_palindrome(newstr))
+ """
+
+
+
+def ispalindrome(userinput):
+    if userinput == userinput[::-1]:
+        return True
+    else:
+        return False
+
+
+go = True
+while(go):
+    userinput = input("Enter a string, to check wether it's a palindrome or not: (type exit to quit)")
+    if userinput == "exit":
+        go = False
+        break
+    userinput == userinput.lower()
+
+    userinputChecked = ""
+    for i in userinput:
+        if(i.isalnum()):
+            userinputChecked+=i
+
+
+    print("Palindrome checking: ", ispalindrome(userinputChecked))
+
--- exceptions_start.py
+++ exceptions_start.py
@@ -6,10 +6,34 @@
 # Errors can happen in programs, and we need a clean way to handle them
 # TODO: This code will cause an error because you can't divide by zero:
 
+#x = 20 / 0
+
+
 
 # TODO: Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
+try:
+    x = 20/0
+except:
+    print("Error")
+
 
 
 # TODO: You can also catch specific exceptions
+
+
+try:
+    answer = input("What number should I devide 10 by ?")
+    num = int(answer)
+    print(10/num)
+except ZeroDivisionError as e:
+    print("Number can't be devided by zero!")
+except ValueError as e:
+     print("Wrong input!")
+     print(e)
+finally:
+    print("this always runs##")
+
+
+
 
